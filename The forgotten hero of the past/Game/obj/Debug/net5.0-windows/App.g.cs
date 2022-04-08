4afa6f38c8734fb2a43ac57091d3e83539ab73b5
aron8132 <csobotharon@gmail.com>
2022-04-08 23:54:00 +0200

    Main Menu restructure + Design

--- The forgotten hero of the past/Game/obj/Debug/net5.0-windows/App.g.cs
+++ The forgotten hero of the past/Game/obj/Debug/net5.0-windows/App.g.cs
@@ -45,7 +45,7 @@ namespace Game {
         /// InitializeComponent
         /// </summary>
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "5.0.12.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "6.0.3.0")]
         public void InitializeComponent() {
             
             #line 5 "..\..\..\App.xaml"
@@ -60,7 +60,7 @@ namespace Game {
         /// </summary>
         [System.STAThreadAttribute()]
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "5.0.12.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "6.0.3.0")]
         public static void Main() {
             Game.App app = new Game.App();
             app.InitializeComponent();

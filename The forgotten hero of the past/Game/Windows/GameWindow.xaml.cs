5cc745a16a4d1955160c188bbb60222935264a91
aron8132 <csobotharon@gmail.com>
2022-05-01 18:05:12 +0200

    Background

--- The forgotten hero of the past/Game/Windows/GameWindow.xaml.cs
+++ The forgotten hero of the past/Game/Windows/GameWindow.xaml.cs
@@ -24,5 +24,10 @@ namespace Game.Windows
         {
             InitializeComponent();
         }
+
+        private void Window_KeyDown(object sender, KeyEventArgs e)
+        {
+
+        }
     }
 }

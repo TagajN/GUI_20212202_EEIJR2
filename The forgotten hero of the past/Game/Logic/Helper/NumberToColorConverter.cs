aef6ea1ecc7acecc2c4b10b230fcfbf3695d6215
aron8132 <csobotharon@gmail.com>
2022-05-05 21:01:17 +0200

    HP Bar Colour

---
+++ The forgotten hero of the past/Game/Logic/Helper/NumberToColorConverter.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace Game.Logic.Helper
+{
+    public class NumberToColorConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            int number = int.Parse(value.ToString());
+            if (number <= 35)
+            {
+                return Brushes.Red;
+            }
+            else if (number <= 50)
+            {
+                return Brushes.Orange;
+            }
+            else if (number <= 75)
+            {
+                return Brushes.Yellow;
+            }
+            else
+            {
+                return Brushes.Green;
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return Binding.DoNothing;
+        }
+    }
+}
